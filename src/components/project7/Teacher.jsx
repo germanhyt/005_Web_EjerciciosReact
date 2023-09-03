027b109245969fa6d29c65b841b37ce70a82ae74
Germán Huaytalla <germanhuaytalla22@gmail.com>
2023-09-02 20:18:07 -0500

    Add files all

--- src/components/project7/Teacher.jsx
+++ src/components/project7/Teacher.jsx
@@ -1,12 +1,33 @@
 import React, { useContext } from 'react'
 import MyContext from '../../context/MyContext'
+import imgAimee from "../../assets/img/aimee.PNG";
+import imgGrace from "../../assets/img/grace.PNG";
+import imgMarta from "../../assets/img/marta.PNG";
 
 //Funciones
 function getImgUrl(fileName) {
     // const imgUrl = new URL("../../assets/img/" + fileName, import.meta.url).href;
-    const imgUrl = new URL("../../../public/img/" + fileName, import.meta.url).href;
+    let imgUrl = '';
+    const arrayTeachers = [imgAimee, imgGrace, imgMarta];
 
-    // console.log(imgUrl);
+    arrayTeachers.forEach((e) => {
+        // console.log(e)
+        let partesRuta = e.split('/');
+        let ultimaParte = partesRuta[partesRuta.length - 1];
+        // console.log(nombre[nombre.length - 1])
+        if (ultimaParte === fileName) {
+            let nombre = ultimaParte.split('.')[0];
+
+            if (nombre == 'aimee') {
+                imgUrl = imgAimee;
+            } else if (nombre === 'grace') {
+                imgUrl = imgGrace;
+            } else if (nombre === 'marta') {
+                imgUrl = imgMarta;
+            }
+        }
+    });
+    // console.log(imgUrl)
     return imgUrl;
 }
 
